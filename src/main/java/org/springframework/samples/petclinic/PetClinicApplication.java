4f53a93c428017a93bc985b3d801d99a003e1cd5
snimo <sebastian_nimo@hotmail.com>
2023-06-18 12:41:52 -0300

    Prueba

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * PetClinic Spring Boot Application.
  *
- * @author Dave Syer
+ * @author Dave Syer / Snimo
  *
  */
 @SpringBootApplication(proxyBeanMethods = false)
