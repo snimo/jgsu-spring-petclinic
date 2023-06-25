3912b30edfd42f6336bfc1335c850a07c982783a
snimo <sebastian_nimo@hotmail.com>
2023-06-24 23:30:33 -0300

    Port change

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * PetClinic Spring Boot Application.
  *
- * @author Dave Syer / Snimo
+ * @author Dave Syer / Snimo 123
  *
  */
 @SpringBootApplication(proxyBeanMethods = false)
